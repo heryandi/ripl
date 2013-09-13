c9f78b4f7935836b55507f3bb7fba2abae20f7e0
Heryandi <heryandi@gmail.com>
2013-09-13 15:36:09 +0800

    Add classifier

--- setup.py
+++ setup.py
@@ -11,17 +11,20 @@ setup(
     author_email='brandonh@stanford.edu',
     packages=find_packages(exclude='test'),
     long_description=open('README').read(),
-      classifiers=[
-          "License :: OSI Approved :: GNU General Public License (GPL)",
-          "Programming Language :: Python",
-          "Development Status :: 4 - Beta",
-          "Intended Audience :: Developers",
-          "Topic :: Internet",
-      ],
-      keywords='networking protocol Internet OpenFlow data center datacenter',
-      license='GPL',
-      install_requires=[
+    classifiers=[
+        "Mininet :: Module :: Topology",
+        "Mininet :: Module :: Others",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Programming Language :: Python",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Internet",
+    ],
+    keywords='networking protocol Internet OpenFlow data center datacenter',
+    license='GPL',
+    install_requires=[
         'setuptools',
         'mininet',
         'networkx'
-      ])
+    ]
+)
